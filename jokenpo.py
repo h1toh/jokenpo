fd672a40a47af4b9855fb2e003f9a51ec85ed490
Lucas Gomes <lukashitoh@gmail.com>
2024-08-31 22:12:54 -0300

    Update jokenpo.py
    v2

--- jokenpo.py
+++ jokenpo.py
@@ -1,19 +1,14 @@
 from random import randint
-
 choices = ["pedra", "papel", "tesoura"]
-
 print("Pedra, Papel ou Tesoura.")
 print("")
-
 # Escolha do player
 while True:
     playerChoice = str(input("Eu escolho: ")).lower()
     if playerChoice in choices:
         break
-
 # Escolha do bot
 botChoice = choices[randint(0,2)]
-
 # Mostrar escolhas
 def showChoices():
     print("")
@@ -22,30 +17,22 @@ def showChoices():
     print("")
     result()
     print("")
-
 def result():
     # Pedra e tesoura
     if playerChoice == choices[0] and botChoice == choices[2]:
         print("Player venceu a partida!")
     elif playerChoice == choices[2] and botChoice == choices[0]:
         print("Bot venceu a partida!")
-    elif playerChoice == choices[0] and botChoice == choices[0]:
-        print("Empate!")
-    
     # Papel e pedra
     if playerChoice == choices[1] and botChoice == choices[0]:
         print("Player venceu a partida!")
     elif playerChoice == choices[0] and botChoice == choices[1]:
         print("Bot venceu a partida!")
-    elif playerChoice == choices[1] and botChoice == choices[1]:
-        print("Empate!")
-
     # Tesoura e papel
     if playerChoice == choices[2] and botChoice == choices[1]:
         print("Player venceu a partida!")
     elif playerChoice == choices[1] and botChoice == choices[2]:
         print("Bot venceu a partida!")
-    elif playerChoice == choices[2] and botChoice == choices[2]:
+    elif playerChoice == botChoice:
         print("Empate!")
-
 showChoices()
